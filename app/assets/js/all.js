f64f74573eeb2694fe4fdca1b8bc25ab143ce988
ToroChu <whileonrails@gmail.com>
2021-07-10 10:18:17 +0800

    add active link

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,5 @@
-console.log('Hello!');
-
 $(document).ready(() => {
-  console.log('Document is ready');
+  const currentPath = location.pathname.split('.')[0].replace('/', '')
+  const currentPageLink = document.querySelector(`#${currentPath}-link`)
+  currentPageLink.classList.toggle('active');
 });
