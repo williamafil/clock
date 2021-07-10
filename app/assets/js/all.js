37037073f8cb4fa2d465f89f57038485d23c3173
ToroChu <whileonrails@gmail.com>
2021-07-10 10:09:27 +0800

    add ability to remove clock

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,12 +1,5 @@
 console.log('Hello!');
 
 $(document).ready(() => {
-  console.log('HesSchool Hello!');
-});
-
-document.querySelectorAll('.nav-link').forEach((link) => {
-  link.addEventListener('click', function() {
-    console.log('this link: ', this)
-    console.log(link)
-  })
+  console.log('Document is ready');
 });
